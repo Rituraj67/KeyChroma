dc9bc6c23e55bef7b323a7f708464b0ea3124543
Rituraj67 <rituraj672000@gmail.com>
2024-11-07 00:55:15 +0530

    ok

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,21 @@ const dbName = "CryptoApp";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+
+const allowedOrigins = ['https://keychroma.vercel.app', 'http://localhost:5173'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 const port = 3000;
